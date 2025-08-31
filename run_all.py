cdbf09a6de2cd4ac94aa7282451330508e3ef65e
LucasCA-GIT <lucascardosoalecrim99@gmail.com>
2025-08-31 17:36:14 -0300

    Script em python para automatizar desde o delete até restaurar todo o deploy

---
+++ run_all.py
@@ -0,0 +1,20 @@
+import subprocess
+
+commands = [
+    "minikube delete --all",
+    "docker system prune -a --volumes -f",
+    "docker build -t lucascalecrim/node-api:lts .",
+    "docker push lucascalecrim/node-api:lts",
+    "ansible-playbook -i ansible/hosts.ini ansible/build-and-push.yml",
+    "minikube start --driver=kvm2",
+    "kubectl create configmap node-api-env --from-file=.env --dry-run=client -o yaml | kubectl apply -f -",
+    "kubectl apply -f k8s/",
+    "watch -n 1 kubectl get pods"
+]
+
+for cmd in commands:
+    print(f"\nExecutando: {cmd}\n{'='*40}")
+    result = subprocess.run(cmd, shell=True)
+    if result.returncode != 0:
+        print(f"Erro ao executar: {cmd}")
+        break
